26a316eeae0aa78fa1ba544c5f78beff06965bb9
Admin <elenaver1984@ya.ru>
2023-05-23 10:21:18 +0500

    Task06

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿
+int number=Convert.ToInt32(Console.ReadLine());
+
+
+if (number%2==0)
+{
+    Console.WriteLine($"{number}, chetnoe");
+}
+else
+{
+    Console.WriteLine($"{number}, nechetnoe");
+}
